cb9c302d7602e5cf00b7521790c2afe3363a47ff
sangwondee <sangwondee@gmail.com>
2019-05-16 10:55:15 +0700

    can make Factory and seeder

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Company;
+use App\Customer;
+use Faker\Generator as Faker;
+
+$factory->define(Customer::class, function (Faker $faker) {
+    return [
+        'company_id' => factory(Company::class)->create(),
+        'name' => $faker->firstName,
+        'email' => $faker->email,
+        'active' => 1
+    ];
+});
